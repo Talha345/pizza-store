6da42cb312369eb68f409bec3a9dac26c60b53a0
Talha Mahmood <talha.mahmood@NB002754.local>
2023-07-25 20:47:24 +0200

    Pizza and Ingredient Models added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+pizzas = {
+  'Margherita': 5.0,
+  'Salami': 6.0,
+  'Tonno': 8.0
+}
+
+pizzas.each do |k, v|
+  Pizza.create(name: k, price: v)
+end
+
+ingredients = {
+  'Onions': 1,
+  'Cheese': 2,
+  'Olives': 2.5,
+}
+
+ingredients.each do |k, v|
+  Ingredient.create(name: k, price: v)
+end
